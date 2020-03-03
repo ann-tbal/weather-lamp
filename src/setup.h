3138e596419fc3a7ab45d74867e154ef957d39ca
ann-tbal <[amtbaldonasa@gmail.com]>
2020-03-03 15:44:54 -0500

    initializing query + created query URL

--- src/setup.h
+++ src/setup.h
@@ -3,20 +3,28 @@
 #include <string.h>
 #include <curl/curl.h>
 
+#define MAX_APP_ID_LEN 40
+#define MAX_CITY_NAME_LEN 20
+#define MIN_QUERY_STR_LEN 50
+#define MAX_FILE_NAME_LEN 20
+
+static const char *temp_param = "temp";
+static char *url = "http://api.openweathermap.org/data/2.5/weather?";
 
 typedef struct {
-    char *file_name;
-    char *app_id;
+    char file_name[MAX_FILE_NAME_LEN];
+    char app_id[MAX_APP_ID_LEN];
+    int app_id_len; 
 } USER;
 
 typedef struct {
-    char *city_name;
+    char city_name[MAX_CITY_NAME_LEN];
+    int city_name_len;
     int temp;
 } CITY; 
 
 typedef struct {
-    char *query_str;
-    char *param; 
-    USER user;
-    CITY city; 
+    char request_url[MIN_QUERY_STR_LEN + MAX_CITY_NAME_LEN + MAX_APP_ID_LEN];
+    USER *user;
+    CITY *city; 
 } QUERY;
